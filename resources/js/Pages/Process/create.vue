ae20ed131922d92a4db7c617895d631092d0825c
ardy-priyo <priyo.ardy@hotmail.com>
2026-06-15 16:52:56 +0700

    gak ada

--- resources/js/Pages/Process/create.vue
+++ resources/js/Pages/Process/create.vue
@@ -21,8 +21,8 @@ const cancelForm = () => {
         <div class="flex-1 flex flex-col min-w-0 h-screen overflow-y-auto">
             <div class="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4 mb-8">
                 <div>
-                    <h1 class="text-2xl font-black text-slate-900 tracking-tight">Create PMFEA Process Function list</h1>
-                    <p class="text-xs text-slate-500 mt-1">Create new PMFEA template process function list.</p>
+                    <h1 class="text-2xl font-black text-slate-900 tracking-tight">Create PMFEA Process Function</h1>
+                    <p class="text-xs text-slate-500 mt-1">Create new PMFEA template process function.</p>
                 </div>
                 <div class="flex items-center gap-3">
                     <Link href="/process" class="inline-flex items-center gap-2 px-4 py-2.5 bg-blue-600 hover:bg-blue-700 text-white font-bold text-xs rounded-xl shadow-md transition active:scale-95 active:bg-slate-200">
