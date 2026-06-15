4c6e0116de0f382a64b07d9b3df64a8f55b1c1f6
ardy-priyo <priyo.ardy@hotmail.com>
2026-06-15 16:51:06 +0700

    Fixing sidebar

--- resources/js/components/Dashboard/Sidebar.vue
+++ resources/js/components/Dashboard/Sidebar.vue
@@ -28,7 +28,7 @@ const toggleProcessList = () => {
             <span
                 class="text-sm font-black tracking-widest text-white uppercase"
             >
-                SCHLEMMER <span class="text-blue-500 font-normal">CORE</span>
+                PMFEA <span class="text-blue-500 font-normal">CORE</span>
             </span>
         </div>
 
@@ -84,14 +84,18 @@ const toggleProcessList = () => {
                     >
                         <Link
                                 href="/process"
-                                class="flex items-center px-3 py-2 rounded-lg text-xs font-medium transition duration-150"
+                                class="flex items-center gap-3 px-3 py-2 rounded-lg text-xs font-medium transition duration-150"
                                 :class="
                                     currentPath === '/process'
                                         ? 'text-white font-bold bg-slate-800/60'
                                         : 'text-slate-400 hover:text-white hover:bg-slate-800/20'
                                 "
                             >
-                                Process list
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6.429 9.75 2.25 12l4.179 2.25m0-4.5 5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0 4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0-5.571 3-5.571-3" />
+                                </svg>
+
+                                <span>Process Template</span>
                             </Link>
                     </div>
                 </div>
